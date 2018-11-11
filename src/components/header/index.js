a0adabb91ab1fe8f85b203a50ccc0bf717019ecb
Luke Morton <lukemorton.dev@gmail.com>
2018-11-11 13:56:24 +0000

    Add Made Tech logo

---
+++ src/components/header/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import logo from '@madetech/marketing-assets/logos/made-tech-logo-colour.png'
+
+export default function Header () {
+  return (
+    <header>
+      <img alt="Made Tech" itemprop="logo" src={logo} width="200px" />
+    </header>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Components from './components'
 
 function App () {
-  return <main>Hello</main>
+  return (
+    <main>
+      <Components.Header />
+    </main>
+  )
 }
 
 const rootElement = document.getElementById('root')
