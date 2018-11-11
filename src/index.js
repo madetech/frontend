12e00eab1618aa93ed165ef5208b3e44f8bab2dc
Luke Morton <lukemorton.dev@gmail.com>
2018-11-11 13:45:57 +0000

    Add CRA with Sass

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+function App () {
+  return <main>Hello</main>
+}
+
+const rootElement = document.getElementById('root')
+ReactDOM.render(<App />, rootElement)
