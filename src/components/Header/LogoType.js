3646b1822cb17bee19986069675ee786c6e2f385
Luke Morton <lukemorton.dev@gmail.com>
2018-11-15 18:31:29 +0000

    Add .header_logo_type outer class

--- src/components/Header/LogoType.js
+++ src/components/Header/LogoType.js
@@ -5,9 +5,9 @@ export default function LogoType ({ text }) {
   const textWithoutSpaces = text.replace(' ', '\u200A')
 
   return (
-    <div class="d-flex align-items-center">
-      <span class="header_logo_type__text">{textWithoutSpaces}</span>
-      <span class="px-3">by</span>
+    <div className='header_logo_type'>
+      <span className='header_logo_type__text'>{textWithoutSpaces}</span>
+      <span className='px-3'>by</span>
       <Logo width='120px' />
     </div>
   )
