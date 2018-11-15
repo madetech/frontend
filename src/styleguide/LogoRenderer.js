1f8dd8a23cbe6f3c9872668582f5da454e3ef86b
Luke Morton <lukemorton.dev@gmail.com>
2018-11-15 18:44:48 +0000

    Fix logo in styleguide

--- src/styleguide/LogoRenderer.js
+++ src/styleguide/LogoRenderer.js
@@ -3,10 +3,10 @@ import Logo from '../components/Header/Logo'
 
 export default function LogoRenderer () {
   return (
-    <div className='header_logo_type' style={{ width: '160px' }}>
-      <span className="header_logo_type__text">Frontend</span>
-
-      <br />
+    <div style={{ width: '160px' }}>
+      <div className='header_logo_type'>
+        <span className='header_logo_type__text'>Frontend</span>
+      </div>
 
       <div class='d-flex align-items-center'>
         <span className="pr-2">by</span>
