10888fa354750b7cf27c0ff9562746b51c8f012d
Luke Morton <lukemorton.dev@gmail.com>
2018-11-17 11:24:33 +0000

    Update name of styleguide

--- src/styleguide/LogoRenderer.js
+++ src/styleguide/LogoRenderer.js
@@ -5,10 +5,10 @@ export default function LogoRenderer () {
   return (
     <div style={{ width: '160px' }}>
       <div className='header_logo_type'>
-        <span className='header_logo_type__text'>Frontend</span>
+        <span className='header_logo_type__text' style={{ lineHeight: '1.3' }}>Frontend Styleguide</span>
       </div>
 
-      <div class='d-flex align-items-center'>
+      <div class='d-flex align-items-center mt-2'>
         <span className="pr-2">by</span>
 
         <Logo width='100px' />
