01b1990aa4ae520ed8da64c060a8b42c1038a09c
Luke Morton <lukemorton.dev@gmail.com>
2018-11-17 01:24:34 +0000

    Add create react app example

---
+++ examples/create-react-app/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Header } from '@madetech/frontend'
+import '@madetech/frontend/dist/css/madetech-frontend.css'
+
+export default function App () {
+  return (
+    <Header />
+  )
+}
