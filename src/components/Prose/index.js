c839ca0116663c41b2590b393441fd0ec31f3471
Luke Morton <lukemorton.dev@gmail.com>
2018-12-23 21:45:54 +0000

    Add Prose component

---
+++ src/components/Prose/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Prose ({ children }) {
+  return (
+    <div className='prose'>
+      {children}
+    </div>
+  )
+
+}
--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 import Footer from './Footer'
 import Header from './Header'
 import Pagination from './Pagination'
+import Prose from './Prose'
 import TopBar from './TopBar'
 
-export { Footer, Header, Pagination, TopBar }
+export { Footer, Header, Pagination, Prose, TopBar }
