1113ce4a6cc80bd5bce4b27424e424a895068ef8
Luke Morton <lukemorton.dev@gmail.com>
2019-01-07 17:34:48 +0000

    Ensure Prose exported

--- src/index.test.js
+++ src/index.test.js
@@ -1,8 +1,9 @@
-import { Footer, Header, Pagination, TopBar } from './index'
+import { Footer, Header, Pagination, Prose, TopBar } from './index'
 
 it('exports components', () => {
   expect(Footer).toBeTruthy()
   expect(Header).toBeTruthy()
   expect(Pagination).toBeTruthy()
+  expect(Prose).toBeTruthy()
   expect(TopBar).toBeTruthy()
 })
