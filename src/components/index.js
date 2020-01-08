2037592a0c04cedce9df6d29df8759b2a955768d
Ninamma <ninamma@madetech.com>
2020-01-08 12:07:42 +0000

    Adding hot-loader as a dependency, so that the alias in site will work and will allow local development of frontend alongside site

--- src/components/index.js
+++ src/components/index.js
@@ -5,5 +5,6 @@ import Pagination from './Pagination'
 import Prose from './Prose'
 import SiteMap from './SiteMap'
 import TopBar from './TopBar'
+import Hero from './Hero'
 
-export { Footer, Header, Jumbotron, Pagination, Prose, SiteMap, TopBar }
+export { Footer, Header, Jumbotron, Pagination, Prose, SiteMap, TopBar, Hero }
