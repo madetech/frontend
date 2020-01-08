aa0c52e64cbbaaf6025918ec739d706a695bfeb6
kosiakkatrina <54268893+kosiakkatrina@users.noreply.github.com>
2020-01-08 09:33:01 +0000

    Adding hero component (#9)
    * adding a new hero component and copying all the hero scss into _hero.scss
    
    * adding most of hero scss (except responsiveness) and importing hero into all.scss
    
    * reverting changes in package.json and package-lock.json files

--- src/components/index.js
+++ src/components/index.js
@@ -3,7 +3,8 @@ import Header from './Header'
 import Jumbotron from './Jumbotron'
 import Pagination from './Pagination'
 import Prose from './Prose'
+import Hero from './Hero'
 import SiteMap from './SiteMap'
 import TopBar from './TopBar'
 
-export { Footer, Header, Jumbotron, Pagination, Prose, SiteMap, TopBar }
+export { Footer, Header, Jumbotron, Pagination, Prose, Hero, SiteMap, TopBar }
