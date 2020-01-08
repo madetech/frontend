2b16a01ae38df40a5696cce82a869e8b681d9cd2
Maria Astakhova <maria@madetech.com>
2020-01-08 13:08:39 +0000

    removing css from hero

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Hero ({ children }) {
   return (
-    <div className={`hero ${children.backgroundColour}`}>
+    <div className='hero'>
       {children}
     </div>
   )
