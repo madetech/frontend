c8764995695e5df74b2e09412979f46bcaadf80d
Ninamma <ninamma@madetech.com>
2020-03-03 17:28:57 +0000

    Allowing Logo to be exported

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,11 @@
 import Footer from './Footer'
 import Header from './Header'
 import Jumbotron from './Jumbotron'
+import Logo from './Header/Logo'
 import Pagination from './Pagination'
 import Prose from './Prose'
 import Hero from './Hero'
 import SiteMap from './SiteMap'
 import TopBar from './TopBar'
 
-export { Footer, Header, Jumbotron, Pagination, Prose, Hero, SiteMap, TopBar }
+export { Footer, Header, Jumbotron, Logo, Pagination, Prose, Hero, SiteMap, TopBar }
