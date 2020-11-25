387aa423bc612c9b3635f3abe9015c93aee33396
bacancy-archita <46159588+bacancy-archita@users.noreply.github.com>
2020-11-25 12:37:04 +0530

    site map and footer components modified as per the requirement of customisable footer and site map links from the contentful (#27)

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,4 @@
 import Footer from './Footer'
-import FooterDynamic from './FooterDynamic'
 import Header from './Header'
 import Jumbotron from './Jumbotron'
 import Logo from './Header/Logo'
@@ -9,4 +8,4 @@ import Hero from './Hero'
 import SiteMap from './SiteMap'
 import TopBar from './TopBar'
 
-export { Footer, Header, Jumbotron, Logo, Pagination, Prose, Hero, SiteMap, TopBar, FooterDynamic }
+export { Footer, Header, Jumbotron, Logo, Pagination, Prose, Hero, SiteMap, TopBar }
